103c9bf42c1862077fd3a1165910558f675105bf
vignesh <vigneshm29022gmail.com>
2022-07-18 10:12:28 +0530

    navbar changes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,6 +3,11 @@ export const Navbar = () => {
   return (
     <div className="Navbar">
       <img src="https://d.novoresume.com/nvlib/images/lg.png" />
+      <div class="link">
+      <a href="#" class="Resume_Templates    ">Resume Templates </a>
+      <a href="#" class="CV_Templates     ">CV Templates </a>
+      <a href="#" class="Cover_Letters   ">Cover Letters </a>
+      </div>
       <div className="header">
         <button class="Registerbutton">Register</button>
         <button class="Signinbutton">Sign In</button>
