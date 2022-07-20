358c1cbc61de9e4a96a9d02674da34371308b955
vignesh <vigneshm29022gmail.com>
2022-07-20 10:39:59 +0530

    resume templats changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import {Navbar} from './components/navbar/Navbar'
 import "./App.css";
 import VideoOverlay from './components/VideoOverlay/VideoOverlay';
 import HomePage from './pages/HomePage';
+import {ResumeTemplates} from './components/Resume_Templates/ResumeTemplates.jsx'
 
 const App = () => {
   return (
     <>
     <Navbar/>
     {/* <VideoOverlay /> */}
-      <HomePage />
+    {/* <HomePage /> */}
+    <ResumeTemplates/>
     </>
   )
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,12 +7,12 @@ export const Navbar = () => {
           <img src="https://d.novoresume.com/nvlib/images/lgt.png" class="Logo2"/>
       </div>
       <div class="Header_Right">
-          <a href="#" class="Resume_Templates">Resume Templates </a>
-          <a href="#" class="CV_Templates">CV Templates </a>
-          <a href="#" class="Cover_Letters">Cover Letters </a>
-          <a href='#' class="Career_Blog">Career Blog</a>
-          <a href='#' class="Pricing">Pricing</a>
-          <a href='#' class="EN_US">EN US</a>
+          <a href="/#" class="Resume_Templates">Resume Templates </a>
+          <a href="/#" class="CV_Templates">CV Templates </a>
+          <a href="/#" class="Cover_Letters">Cover Letters </a>
+          <a href='/#' class="Career_Blog">Career Blog</a>
+          <a href='/#' class="Pricing">Pricing</a>
+          <a href='/#' class="EN_US">EN US</a>
           <button class="Registerbutton">Register</button>
           <button class="Signinbutton">Sign In</button>
       </div>
