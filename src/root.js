9d3b42bf7404d5db3f7f775995ab9173d1de7074
Rahul krishnan <rahul.r5@myntra.com>
2022-07-21 21:41:34 +0530

    Changes

--- src/root.js
+++ src/root.js
@@ -5,12 +5,14 @@ import VideoOverlay from "./components/VideoOverlay/VideoOverlay";
 
 const Root = ({ children, showVideoAd }) => {
   return (
-    <React.Suspense fallback={"Loading"}>
-      <BrowserRouter>
-        <Navbar />
-        {showVideoAd ? <VideoOverlay /> : children}
-      </BrowserRouter>
-    </React.Suspense>
+    <ErrorBoundary>
+      <React.Suspense fallback={"Loading"}>
+        <BrowserRouter>
+          <Navbar />
+          {showVideoAd ? <VideoOverlay /> : children}
+        </BrowserRouter>
+      </React.Suspense>
+    </ErrorBoundary>
   );
 };
 
