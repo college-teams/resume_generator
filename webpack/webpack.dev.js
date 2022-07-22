22408d4deadd69e8ab4387223d99846034bd9005
Rahul krishnan <rahul.r5@myntra.com>
2022-07-22 21:50:54 +0530

    changes

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -24,7 +24,6 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      'process.env.MY_ENV': JSON.stringify(process.env.MY_ENV),
       'process.env.PUBLIC_URL': JSON.stringify(process.env.PUBLIC_URL),
     })
   ],
