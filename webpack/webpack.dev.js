fa20d7339c1f59ee99d52081c50d3631948da8a1
Rahul krishnan <rahul.r5@myntra.com>
2022-07-22 21:57:14 +0530

    changes

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -22,10 +22,9 @@ module.exports = {
       template: "./src/template/index.html",
       // favicon: commonPaths.faviconPath,
     }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      'process.env.PUBLIC_URL': JSON.stringify(process.env.PUBLIC_URL),
-    })
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
   ],
   devServer: {
     historyApiFallback: true,
